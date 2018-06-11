0490ffd78bedfc560d07c746d1ffd665735275d8
Yu Sun <yu.sun.cs@gmail.com>
2018-06-11 11:01:46 -0700

    update

--- product-pricing.py
+++ product-pricing.py
@@ -1,4 +1,5 @@
 import datetime
+import smtplib
 
 pricing_map = {}
 pricing_map["audio glass"] = 249.99
@@ -40,6 +41,34 @@ def generate_email_body(place, name, company, school, email, product_list):
                  "The total amount of your order is $" + str(get_all_product_price(product_list)) + ". Please make your payment within 30 days. Thank you so much for the business. We hope to see you again! \n\n" + \
                  "Audio Glass Sales Department \n" + datetime.date.today().strftime("%B %d, %Y")
     return email_body
+
+def send_email(email, email_body):
+    gmail_user = 'yu.sun.eusa@gmail.com'
+    gmail_password = 'Justdoit!'
+
+    sent_from = gmail_user
+    to = ['lihoward07@gmail.com']
+    subject = 'Alloy Cube'
+    body = 'Your Sales Order is Here Finally'
+
+    email_text = """\ 
+    From: %s  
+    To: %s  
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+        print('Email sent!')
+
+    except:
+        print('Something went wrong...')
 
 print(generate_email_body("San Diego", "Howard Li", "Audio Glasses Inc.", "Univ HS", "howardli07@gmail.com", product_list))
 
